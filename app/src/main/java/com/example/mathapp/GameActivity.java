2f228a1e347953908c9240137dcb08893cba5ee6
Eugene Vetrov <evg.vetrow@yandex.ru>
2023-07-09 16:56:45 +0500

    Game core complete

--- app/src/main/java/com/example/mathapp/GameActivity.java
+++ app/src/main/java/com/example/mathapp/GameActivity.java
@@ -8,6 +8,8 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.Random;
+
 public class GameActivity extends AppCompatActivity implements View.OnClickListener {
 
     int correctAnswer;
@@ -26,8 +28,8 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
-        TextView textObjectPartA = (TextView) findViewById(R.id.textPartA);
-        TextView textObjectPartB = (TextView) findViewById(R.id.textPartB);
+        textObjectPartA = (TextView) findViewById(R.id.textPartA);
+        textObjectPartB = (TextView) findViewById(R.id.textPartB);
         textObjectScore = (TextView)findViewById(R.id.textScore);
         textObjectLevel = (TextView)findViewById(R.id.textLevel);
         buttonObjectChoice1 = (Button) findViewById(R.id.buttonChoice1);
@@ -37,11 +39,12 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
         buttonObjectChoice1.setOnClickListener(this);
         buttonObjectChoice2.setOnClickListener(this);
         buttonObjectChoice3.setOnClickListener(this);
+        setQuestion();
     }
 
     @Override
     public void onClick(View view){
-        int answerGiven;
+        int answerGiven = 0;
         if(view.getId() == R.id.buttonChoice1){
             answerGiven = Integer.parseInt("" + buttonObjectChoice1.getText());
         } else if(view.getId() == R.id.buttonChoice2){
@@ -49,5 +52,65 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
         } else if(view.getId() == R.id.buttonChoice3){
             answerGiven = Integer.parseInt("" + buttonObjectChoice3.getText());
         }
+        updateScoreAndLevel(answerGiven);
+        setQuestion();
+    }
+
+    void setQuestion() {
+        int numberRange = currentLevel * 3;
+        Random randInt = new Random();
+        int partA = randInt.nextInt(numberRange);
+        partA++;
+        int partB = randInt.nextInt(numberRange);
+        partB++;
+        correctAnswer = partA * partB;
+        int wrongAnswer1 = correctAnswer-2;
+        int wrongAnswer2 = correctAnswer+2;
+        textObjectPartA.setText(String.valueOf(partA));
+        textObjectPartB.setText(String.valueOf(partB));
+        int buttonLayout = randInt.nextInt(3);
+        switch (buttonLayout){
+            case 0:
+                buttonObjectChoice1.setText(String.valueOf(correctAnswer));
+                buttonObjectChoice2.setText(String.valueOf(wrongAnswer1));
+                buttonObjectChoice3.setText(String.valueOf(wrongAnswer2));
+                break;
+            case 1:
+                buttonObjectChoice2.setText(String.valueOf(correctAnswer));
+                buttonObjectChoice3.setText(String.valueOf(wrongAnswer1));
+                buttonObjectChoice1.setText(String.valueOf(wrongAnswer2));
+                break;
+            case 2:
+                buttonObjectChoice3.setText(String.valueOf(correctAnswer));
+                buttonObjectChoice1.setText(String.valueOf(wrongAnswer1));
+                buttonObjectChoice2.setText(String.valueOf(wrongAnswer2));
+                break;
+        }
+    }
+    void updateScoreAndLevel(int answerGiven) {
+        if(isCorrect(answerGiven)){
+            for(int i = 1; i <= currentLevel; i++){
+                currentScore = currentScore + i;
+            }
+            currentLevel++;
+        } else {
+            currentScore = 0;
+            currentLevel = 1;
+        }
+        textObjectScore.setText(String.format("Score: %s", String.valueOf(currentScore)));
+        textObjectLevel.setText(String.format("Level: %s", String.valueOf(currentLevel)));
+    }
+    boolean isCorrect(int answerGiven) {
+        boolean correctTrueOrFalse;
+        if(answerGiven == correctAnswer) {
+            Toast.makeText(getApplicationContext(), "Well done!",
+                    Toast.LENGTH_LONG).show();
+            correctTrueOrFalse=true;
+        } else {
+            Toast.makeText(getApplicationContext(), "Sorry",
+                    Toast.LENGTH_LONG).show();
+            correctTrueOrFalse=false;
+        }
+        return correctTrueOrFalse;
     }
 }
